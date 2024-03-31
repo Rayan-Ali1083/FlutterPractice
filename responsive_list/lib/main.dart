e7580fe0e7150e0603a6cfb4b5944bf68b29530a
Rayan-Ali1083 <rayanali1083@gmail.com>
2024-04-01 02:10:32 +0500

    fetching data

--- responsive_list/lib/main.dart
+++ responsive_list/lib/main.dart
@@ -11,48 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Reponsive UI Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue,
-            title: const Text('Flutter Demo',
-                style: TextStyle(color: Colors.white)),
-          ),
-          body: OrientationBuilder(builder: (context, orientation) {
-            return orientation == Orientation.portrait
-                ? const _buildPortrait()
-                : const _buildLandScape();
-          }),
-        ));
-  }
-}
-
-class _buildPortrait extends StatelessWidget {
-  const _buildPortrait({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) => ListTile(
-          title: Center(child: Text('Item $index')),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Reponsive UI Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title:
+              const Text('Flutter Demo', style: TextStyle(color: Colors.white)),
+        ),
+        body: const Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://m.media-amazon.com/images/I/81Ce6sxskfL.jpg')),
         ),
       ),
     );
-  }
-}
-
-class _buildLandScape extends StatelessWidget {
-  const _buildLandScape({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 10,
-        children:
-            List.generate(20, (index) => Center(child: Text('Item $index'))));
   }
 }
